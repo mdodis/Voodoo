f3bfc0dc67529bd6075678c63ab4ed4e3bf305d8
Michael Dodis <michaeldodisgr@gmail.com>
2023-03-26 14:31:48 +0300

    add descriptor builder

---
+++ Engine/DescriptorLayoutCache.cpp
@@ -0,0 +1,49 @@
+#include "DescriptorLayoutCache.h"
+
+#include "Sort.h"
+
+void DescriptorLayoutCache::init(VkDevice device) { this->device = device; }
+
+VkDescriptorSetLayout DescriptorLayoutCache::create_layout(
+    VkDescriptorSetLayoutCreateInfo* info)
+{
+    DescriptorLayoutCache::DescriptorLayoutInfo layout_info(allocator);
+    int                                         last_binding = -1;
+    bool                                        is_sorted    = true;
+
+    for (u32 i = 0; i < info->bindingCount; ++i) {
+        if (info->pBindings[i].binding > last_binding) {
+            last_binding = info->pBindings[i].binding;
+
+        } else {
+            is_sorted = false;
+        }
+
+        layout_info.bindings.add(info->pBindings[i]);
+    }
+
+    // Sort if not sorted
+    if (!is_sorted) {
+        auto s = slice(layout_info.bindings);
+        sort::quicksort(s);
+    }
+
+    if (layout_cache.contains(layout_info)) {
+        auto result = layout_cache[layout_info];
+        layout_info.release();
+        return result;
+    }
+
+    VkDescriptorSetLayout layout;
+    vkCreateDescriptorSetLayout(device, info, 0, &layout);
+    layout_cache.add(layout_info, layout);
+    return layout;
+}
+
+void DescriptorLayoutCache::deinit()
+{
+    for (auto pair : layout_cache) {
+        pair.key.release();
+        vkDestroyDescriptorSetLayout(device, pair.val, nullptr);
+    }
+}
---
+++ Engine/DescriptorLayoutCache.h
@@ -0,0 +1,78 @@
+#pragma once
+#include "Containers/Map.h"
+#include "Hashing.h"
+#include "VulkanCommon.h"
+
+struct DescriptorLayoutCache {
+    DescriptorLayoutCache(IAllocator& allocator)
+        : layout_cache(allocator), allocator(allocator)
+    {}
+
+    struct DescriptorLayoutInfo {
+        DescriptorLayoutInfo(IAllocator& allocator) : bindings(&allocator) {}
+
+        TArray<VkDescriptorSetLayoutBinding> bindings;
+        void                                 release() { bindings.release(); }
+    };
+
+    void                  init(VkDevice device);
+    VkDescriptorSetLayout create_layout(VkDescriptorSetLayoutCreateInfo* info);
+    void                  deinit();
+
+    IAllocator&                                       allocator;
+    TMap<DescriptorLayoutInfo, VkDescriptorSetLayout> layout_cache;
+    VkDevice                                          device;
+};
+
+static _inline u64 hash_of(
+    const DescriptorLayoutCache::DescriptorLayoutInfo& info, u32 seed)
+{
+    u64 result = hash_of(info.bindings.size, seed);
+
+    for (const auto& binding : info.bindings) {
+        u64 packed =
+            (binding.binding) | (binding.descriptorType << 8) |
+            (binding.descriptorCount << 16) | (binding.stageFlags << 24);
+
+        char* parr = (char*)&packed;
+        u64   hash = murmur_hash2(parr, sizeof(packed), seed);
+
+        result ^= hash;
+    }
+
+    return result;
+}
+
+static _inline bool operator==(
+    const DescriptorLayoutCache::DescriptorLayoutInfo& left,
+    const DescriptorLayoutCache::DescriptorLayoutInfo& right)
+{
+    if (left.bindings.size != right.bindings.size) return false;
+
+    for (u64 i = 0; i < left.bindings.size; ++i) {
+        const VkDescriptorSetLayoutBinding& lb = left.bindings[i];
+        const VkDescriptorSetLayoutBinding& rb = right.bindings[i];
+
+        if (lb.binding != rb.binding) return false;
+        if (lb.descriptorType != rb.descriptorType) return false;
+        if (lb.descriptorCount != rb.descriptorCount) return false;
+        if (lb.stageFlags != rb.stageFlags) return false;
+        if (lb.pImmutableSamplers != rb.pImmutableSamplers) return false;
+    }
+
+    return true;
+}
+
+static _inline bool operator!=(
+    const DescriptorLayoutCache::DescriptorLayoutInfo& left,
+    const DescriptorLayoutCache::DescriptorLayoutInfo& right)
+{
+    return !(left == right);
+}
+
+static _inline bool operator<(
+    const VkDescriptorSetLayoutBinding& left,
+    const VkDescriptorSetLayoutBinding& right)
+{
+    return left.binding < right.binding;
+}
